e06992c15cc589eba1ab4b6ce5d3ae63f15e90d2
victorbc <vbatistacamilo@gmail.com>
2017-02-20 17:50:59 -0300

    Session 3, Lecture 16

--- src/shortner/models.py
+++ src/shortner/models.py
@@ -1,6 +1,23 @@
 from django.db import models
-from .utils import code_generator
+from .utils import create_shortcode
 # Create your models here.
+
+
+
+class KirrURLManager(models.Manager):
+	def all(self,*args,**kwargs):
+		qs_main = super(KirrURLManager, self).all(*args,**kwargs)
+		qs = qs_main.filter(active=False)
+		return qs
+
+	def refresh_shortcodes(self):
+		qs = KirrURL.objects.filter(id__gte=1)
+		new_codes = 0
+		for q in qs:
+			q.shortcode = create_shortcode(self)
+			q.save()
+			new_codes +=1
+		return "New codes made: {i}".format(i=new_codes)
 
 
 
@@ -9,6 +26,7 @@ class KirrURL(models.Model):
 	shortcode = models.CharField(max_length=15, unique=True, blank=True)
 	update = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
+	active = models.BooleanField(default=True)
 
 	def save(self, *args, **kwargs):
 		if self.shortcode is None or self.shortcode == "":
