30a9c3b79bf7e23fc5f3a1d0bf68d376cddc73ca
Victor Batista Camilo <victor.camilo@virtus.ufcg.edu.br>
2017-05-22 17:00:28 -0300

    Section 3, Lecture 27

--- src/shortner/views.py
+++ src/shortner/views.py
@@ -2,12 +2,28 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from .forms import SubmitUrlForm
 from .models import KirrURL
 
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "shortner/home.html", {})
+        form = SubmitUrlForm
+        context = {
+            "title":"Pagina Inicial",
+            "form":form
+        }
+        return render(request, "shortner/home.html", context)
+
+    def post(self, request, *args, **kwargs):
+        form = SubmitUrlForm(request.POST)
+        if form.is_valid():
+            print (form.cleaned_data)
+        context = {
+            "title": "Nova Url",
+            "form": form
+        }
+        return render(request, "shortner/home.html", context)
 
 
 class KirrCBView(View):
