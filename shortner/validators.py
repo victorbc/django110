6f0f745a2cad678bce40a6b49597c8b11ef6e3f2
Victor Batista Camilo <victor.camilo@virtus.ufcg.edu.br>
2017-05-22 17:52:03 -0300

    Section 5, Lecture 38

---
+++ shortner/validators.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator
+
+
+def validate_url(value):
+    url_validator = URLValidator()
+    try:
+        url_validator(value)
+    except ValidationError:
+        try:
+            url_validator('http://' + value)
+        except ValidationError:
+            raise ValidationError('Url invalida')
+    return value
+
+
+def validate_dot_com(value):
+    if 'com' not in value:
+        raise ValidationError('Url não possui .com')
+    return value
--- shortner/views.py
+++ shortner/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
+from analytics.models import ClickEvent
 
 from .forms import SubmitUrlForm
 from .models import KirrURL
@@ -10,7 +11,7 @@ class HomeView(View):
     def get(self, request, *args, **kwargs):
         form = SubmitUrlForm
         context = {
-            "title": "Pagina Inicial",
+            "title": "Encurte sua URL",
             "form": form
         }
         return render(request, "shortner/home.html", context)
@@ -24,6 +25,8 @@ class HomeView(View):
         template = "shortner/home.html"
         if form.is_valid():
             new_url = form.cleaned_data.get('url')
+            if not (new_url.startswith('http://') or new_url.startswith('https://')):
+                new_url = 'http://' + new_url
             obj, created = KirrURL.objects.get_or_create(url=new_url)
             context = {
                 'object': obj,
@@ -37,7 +40,8 @@ class HomeView(View):
         return render(request, template, context)
 
 
-class KirrCBView(View):
+class URLRedirectView(View):
     def get(self, request, shortcode=None, *args, **kwargs):
         obj = get_object_or_404(KirrURL, shortcode=shortcode)
+        ClickEvent.objects.create_event(obj)
         return HttpResponseRedirect(obj.url)
